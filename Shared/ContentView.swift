14dfa58e9ceb05ec65b95a6a7d7794bb48b9b5c9
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-07-05 16:30:53 -0700

    icons + background layout foundation

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,36 +10,73 @@ import UIKit
 
 struct ContentView: View {
     var body: some View {
-        Home()
+        
+        VStack{
+            Home()
     }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            
+            
     }
 }
 
 
-struct Home {
+struct Home: View {
     var body: some View {
     ZStack{
-        Image("testpic")
-   
-      VStack{
+        Color.black.ignoresSafeArea()
+//        Image("testpic")
+        
+        HStack{
+            Spacer()
+            VStack(spacing: 8){
+        Spacer()
         Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Image(systemName: "person")
-        })
+            Image(systemName: "person")
+                .foregroundColor(Color.white)
+            Text("1021")
+        }).padding()
+        
         Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Image(systemName: "person")
-        })
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Image(systemName: "person")
-        })
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-          Image(systemName: "person")
-        })
-      }
-}
+            Image(systemName: "heart").foregroundColor(Color.white)
+            Text("1021")
+        }).padding()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Image(systemName: "message").foregroundColor(Color.white)
+            Text("1021")
+        }).padding()
+                Spacer()
+      }.padding(5)
+        }
+        
+        VStack(spacing: 0.0){
+            Spacer()
+            
+            HStack(spacing: 0){
+               
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "house")
+                    .foregroundColor(Color.white)
+            })
+                Spacer(minLength: 0)
+                
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "magnifyingglass").foregroundColor(Color.white)
+            })
+                Spacer(minLength: 0)
+                
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "message").foregroundColor(Color.white)
+            })
+            }.padding(.horizontal)
+        }
+        
+    }
     }
 }
