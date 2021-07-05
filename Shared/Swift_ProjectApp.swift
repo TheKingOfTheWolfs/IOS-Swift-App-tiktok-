8bb0a244fd0039e0f5194e59082f0dd8ee0e6cf7
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-07-04 19:13:16 -0700

    Initial Commit

---
+++ Shared/Swift_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ProjectApp.swift
+//  Shared
+//
+//  Created by Yury Dzhishkariani on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
