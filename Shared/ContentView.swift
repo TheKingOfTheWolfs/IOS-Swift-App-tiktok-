0acbb6ca20209c39ceaa5d5e8565d922a9a0bb71
Yura Dzhishkariani MAC <Dziyura@gmail.com>
2021-07-06 12:18:26 -0700

    fixed bug + finished nabber layout

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,15 +13,15 @@ struct ContentView: View {
         
         VStack{
             Home()
+        }
     }
-}
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
-            
-            
+        
+        
+        
     }
 }
 
@@ -30,73 +30,119 @@ struct Home: View {
     var body: some View {
         
         //Background
-    ZStack{
-        Color.black.ignoresSafeArea()
-//        Image("testpic")
-        
-        //NavBar rightSide
-        
-        VStack{
+        ZStack{
+            Color.black.ignoresSafeArea()
+            //        Image("testpic")
             
-            HStack{
+            //NavBar rightSide
+            
+            VStack{
+                
+                HStack{
+                    Spacer()
+                    VStack(spacing: 35){
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image("profile")
+                                .renderingMode(.original).resizable().frame(width: 55, height: 55).clipShape(Circle())
+                        }
+                        
+                        Button(action: {
+                            
+                        }) {
+                            
+                            VStack(spacing: 8){
+                                Image(systemName:"suit.heart.fill").foregroundColor(Color.white).font(.title)
+                                Text("22k").foregroundColor(.white)
+                                
+                            }
+                        }
+                        Button(action: {
+                            
+                        }) {
+                            
+                            VStack(spacing: 8){
+                                Image(systemName:"message.fill").foregroundColor(Color.white).font(.title)
+                                Text("22k").foregroundColor(.white)
+                                
+                            }
+                        }
+                    }
+                }
+    
+            
+            }.padding(.bottom, 110.0)
+            
+        
+//NavBar Bottom
+            VStack{
                 Spacer()
-                VStack(spacing: 35){
-                    
+            HStack(){
                 
-                Button(action: {
-                    
-                }) {
-                    Image("profile")
-                        .renderingMode(.original).resizable().frame(width: 55, height: 55).clipShape(Circle())
-          }
-                Button(action: {
-                    
-                }) {
-                    
-                    VStack(spacing: 8){
-                    Image(systemName:"suit.heart.fill").foregroundColor(Color.white).font(.title)
-                        Text("22k").foregroundColor(.white)
                 
-            }
-          }
-                Button(action: {
-                    
-                }) {
-                    
-                    VStack(spacing: 8){
-                    Image(systemName:"message.fill").foregroundColor(Color.white).font(.title)
-                        Text("22k").foregroundColor(.white)
                 
-            }
-          }
-        }
-            }
-        //NavBar Pannel bottom page
-        VStack(spacing: 0.0){
-            Spacer()
-            
-            HStack(spacing: 0){
-               
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Image(systemName: "house")
+                
+                Button(action: {}) {
+                VStack{
+                    
+                    Image(systemName: "house")
                     .foregroundColor(Color.white)
-            })
-                Spacer(minLength: 0)
-                
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Image(systemName: "magnifyingglass").foregroundColor(Color.white)
-            })
-                Spacer(minLength: 0)
-                
+                    Text("Home")
+                    .foregroundColor(Color.white)
+            }
+                }
+                Spacer()
+                Button(action: {}) {
+                VStack{
+                    Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color.white)
+                    Text("Search")
+                    .foregroundColor(Color.white)
+            }
+                }
+                Spacer()
+
+                Button(action: {}) {
+                VStack{
+                    Image(systemName: "bubble.right")
+                    .foregroundColor(Color.white)
+                    Text("Inbox")
+                    .foregroundColor(Color.white)
+            }
+                }
+                Spacer()
+
+                Button(action: {}) {
+                VStack{
+                    Image(systemName: "person")
+                    .foregroundColor(Color.white)
+                    Text("Settings")
+                    .foregroundColor(Color.white)
+            }
+                }
             
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Image(systemName: "message").foregroundColor(Color.white)
-            })
-            }.padding(.horizontal)
-        }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            }//Hstack
+            }//VStack
+        }//ZStack
+                    
         
-    }
-        .padding(.bottom, 55)
-    }
-}
-}
+    }//View
+    
+}//View
+                
+                
+                                
+
+
